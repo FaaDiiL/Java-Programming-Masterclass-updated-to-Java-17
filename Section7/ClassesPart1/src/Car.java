a89e807d472ea357241f8e5c890d329475fb0e91
Fadil Al Farhan <fadil.alfarhan@gmail.com>
2023-03-18 17:00:22 +0100

    added classes for challenge

--- Section7/ClassesPart1/src/Car.java
+++ Section7/ClassesPart1/src/Car.java
@@ -1,3 +1,5 @@
+package Section7.ClassesPart1.src;
+
 public class Car {
     private String make = "Tesla";
     private String model = "Model X";
@@ -8,7 +10,6 @@ public class Car {
 
     public String getMake() {
         return make;
-
     }
 
     public String getModel() {
@@ -25,6 +26,34 @@ public class Car {
 
     public boolean isConvertible() {
         return convertible;
+    }
+
+    public void setMake(String make) {
+        if(make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "porsche", "tesla", "ford", "honda", "toyota" ->
+                this.make = make;
+            default -> {
+                this.make = "Unsupported";
+            }
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
     }
 
     public void describeCar() {
--- Section7/ClassesPart1/src/Main.java
+++ Section7/ClassesPart1/src/Main.java
@@ -1,13 +1,33 @@
+package Section7.ClassesPart1.src;
+
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
-        // car.make = "Porsche";
-        // car.model = "Carrera";
-        // car.color = "Red";
-        System.out.println("make = " + car.getMake());
-        System.out.println("model = " + car.getModel());
-        car.describeCar();
-        car.describeCar();
-        car.describeCar();
+        Car carrera2017 = new Car();
+        carrera2017.setMake("Porsche");
+        carrera2017.setModel("Carrera");
+        carrera2017.setDoors(2);
+        carrera2017.setConvertible(true);
+        carrera2017.setColor("black");
+
+        System.out.println("make = " + carrera2017.getMake());
+        System.out.println("model = " + carrera2017.getModel());
+        carrera2017.describeCar();
+
+        System.out.println("=====================================");
+
+        Car Player2 = new Car();
+        Player2.setMake("Ford");
+        Player2.setModel("Mustang");
+        Player2.setDoors(2);
+        Player2.setConvertible(true);
+        Player2.setColor("red");
+
+        System.out.println("make = " + Player2.getMake());
+        System.out.println("model = " + Player2.getModel());
+        Player2.describeCar();
+
+
+
+
     }
 }
